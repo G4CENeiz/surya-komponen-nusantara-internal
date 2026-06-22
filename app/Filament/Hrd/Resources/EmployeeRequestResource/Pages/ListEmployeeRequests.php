d9649c67e772d2b2facaa5e57d31d08847445a17
Kamila Zahwa <kamilazhwa@gmail.com>
2026-06-22 22:48:55 +0700

    feat: add List page for EmployeeRequest resource

---
+++ app/Filament/Hrd/Resources/EmployeeRequestResource/Pages/ListEmployeeRequests.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Hrd\Resources\EmployeeRequestResource\Pages;
+
+use App\Filament\Hrd\Resources\EmployeeRequestResource;
+use Filament\Resources\Pages\ListRecords;
+
+class ListEmployeeRequests extends ListRecords
+{
+    protected static string $resource = EmployeeRequestResource::class;
+}
